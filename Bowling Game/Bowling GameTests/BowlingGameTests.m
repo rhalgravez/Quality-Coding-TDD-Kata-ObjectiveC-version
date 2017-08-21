4400523934cd3e316a5d2728b35b31a945434e69
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-08-21 17:34:00 -0600

    ❌  Failing test for a game where all rolls the pins were one

--- Bowling Game/Bowling GameTests/BowlingGameTests.m
+++ Bowling Game/Bowling GameTests/BowlingGameTests.m
@@ -16,5 +16,11 @@
     XCTAssertEqual([game score], 0, @"Score should be 0");
 }
 
-
+- (void)testAllOnes {
+    BowlingGame *game = [[BowlingGame alloc] init];
+    for (NSUInteger i = 0; i < 20; i++) {
+        [game rollWithPinCount:1];
+    }
+    XCTAssertEqual([game score], 20, @"Score should be 20");
+}
 @end

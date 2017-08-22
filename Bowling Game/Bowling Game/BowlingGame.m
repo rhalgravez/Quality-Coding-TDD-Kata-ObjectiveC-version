edb47a09cb82474ec8c24627dd7d00b3b3db57aa
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-08-21 18:53:38 -0600

    Uncomment the testOneSpare, and start writting the simplest solution, we find out that the solution might be wrong. Because "i" in the loop might not refer to the first ball of the frame. Need to walk through array two balls (one frame) at a time.
    *Code does not compile*

--- Bowling Game/Bowling Game/BowlingGame.m
+++ Bowling Game/Bowling Game/BowlingGame.m
@@ -12,7 +12,9 @@
 -(NSUInteger)score {
     NSUInteger score = 0;
     for (NSUInteger i = 0; i < 21; i++) {
-        score += _rolls[i];
+        if(_rolls[i] +_rolls[i+1] == 10) {
+            score += ... //Wait...this is not going to work, "i"might not refer to the first ball of the frame
+        }
     }
     return score;
 }

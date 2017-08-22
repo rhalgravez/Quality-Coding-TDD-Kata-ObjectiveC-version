f8c139f1f0f36e5f0555622584f3b76bc111c2a5
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-08-21 18:22:54 -0600

    With previous test (testOneSpare) we find out that the design is wrong. Responsibilities are misplaced.
    Why?
    Because -rollWithPinCount: calculates score, but name does not imply that, and -score does not calculate score, but name implies that it does.
    
    So comment the last test and refactor the methods (-rollWithPinCount: and -score) and check if the changes doesn't affect the previous tests

--- Bowling Game/Bowling Game/BowlingGame.m
+++ Bowling Game/Bowling Game/BowlingGame.m
@@ -2,14 +2,21 @@
 
 @implementation BowlingGame{
     NSUInteger _score;
+    NSUInteger _rolls[21];
+    NSUInteger _currentRoll;
 }
 
 -(void)rollWithPinCount:(NSInteger)pins {
     _score += pins;
+    _rolls[_currentRoll++] = pins;
 }
 
 -(NSUInteger)score {
-    return _score;
+    NSUInteger score = 0;
+    for (NSUInteger i = 0; i < 21; i++) {
+        score += _rolls[i];
+    }
+    return score;
 }
 
 @end

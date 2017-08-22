b95b5d1972cc809272afbc5109d66bdcbb9965ae
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-08-21 19:02:24 -0600

    ♻️  Refactor the design for -scoren so now can go through frames and not rolls like previously. Comment testOneSpare because the refactor to -score can affect the previous tests
    Btw testAllOnes and testGutterGame both succedd

--- Bowling Game/Bowling Game/BowlingGame.m
+++ Bowling Game/Bowling Game/BowlingGame.m
@@ -11,10 +11,10 @@
 
 -(NSUInteger)score {
     NSUInteger score = 0;
-    for (NSUInteger i = 0; i < 21; i++) {
-        if(_rolls[i] +_rolls[i+1] == 10) {
-            score += ... //Wait...this is not going to work, "i"might not refer to the first ball of the frame
-        }
+    NSUInteger i = 0;
+    for (NSUInteger frame = 0; frame < 10; frame++) {
+        score += _rolls[1] + _rolls[i + 1];
+        i += 2;
     }
     return score;
 }

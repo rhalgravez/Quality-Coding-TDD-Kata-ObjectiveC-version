b3f9a008a2d13de6c96f27f924307fcd05cc4154
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-08-21 18:36:49 -0600

    The ivar _score is no longer needed

--- Bowling Game/Bowling Game/BowlingGame.m
+++ Bowling Game/Bowling Game/BowlingGame.m
@@ -1,13 +1,11 @@
 #import "BowlingGame.h"
 
 @implementation BowlingGame{
-    NSUInteger _score;
     NSUInteger _rolls[21];
     NSUInteger _currentRoll;
 }
 
 -(void)rollWithPinCount:(NSInteger)pins {
-    _score += pins;
     _rolls[_currentRoll++] = pins;
 }
 
